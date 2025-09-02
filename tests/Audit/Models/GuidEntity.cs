f2d85cccdae4fcc5183c3a9101e4236e45eab912
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-09-02 12:06:54 +0700

    Refactor XML documentation for consistency and readability across entity classes

--- tests/Audit/Models/GuidEntity.cs
+++ tests/Audit/Models/GuidEntity.cs
@@ -3,16 +3,22 @@
 namespace Wangkanai.Audit.Models;
 
 /// <summary>Represents an entity with a globally unique identifier (GUID) as its primary key.</summary>
-public class GuidEntity : KeyGuidEntity
+public class GuidEntity
+   : KeyGuidEntity
 {
    /// <summary>Represents an entity with a globally unique identifier (GUID) as its primary key.</summary>
-   public GuidEntity() => Id = Guid.NewGuid();
+   public GuidEntity()
+      => Id = Guid.NewGuid();
 }
 
 /// <summary>Represents a transient entity with a globally unique identifier (GUID) as its primary key. This entity is initialized with an empty
 /// <see cref="Guid"/> value, indicating its transient state.</summary>
-public class TransientGuidEntity : KeyGuidEntity
+public class TransientGuidEntity
+   : KeyGuidEntity
 {
-   /// <summary>Represents a transient entity with a globally unique identifier (GUID) as its primary key, initialized to an empty GUID value, indicating a transient or non-persistent state.</summary>
+   /// <summary>
+   /// Represents a transient entity with a globally unique identifier (GUID) as its primary key,
+   /// initialized to an empty GUID value, indicating a transient or non-persistent state.
+   /// </summary>
    public TransientGuidEntity() => Id = Guid.Empty;
 }
