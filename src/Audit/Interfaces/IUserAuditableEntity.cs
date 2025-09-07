181836a8905156d8548c843c8170bb77d17baa4c
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-09-07 15:43:06 +0700

    Refactor IUserAuditableEntity interface and introduce user-specific auditing interfaces for creation, update, and deletion tracking

--- src/Audit/Interfaces/IUserAuditableEntity.cs
+++ src/Audit/Interfaces/IUserAuditableEntity.cs
@@ -3,31 +3,15 @@
 namespace Wangkanai.Audit;
 
 /// <summary>
-/// Defines an interface for auditable entities that include user-related metadata.
-/// This interface extends the <see cref="IAuditableEntity"/> interface and provides additional properties
-/// to track the user responsible for creating, updating, and deleting the entity.
+/// Represents an interface that combines user-specific auditing capabilities for tracking
+/// creation, modification, and deletion activities performed by users.
 /// </summary>
-public interface IUserAuditableEntity : IAuditableEntity
-{
-   /// <summary>
-   /// Gets or sets the identifier of the user who created the entity.
-   /// This property is used for tracking the user responsible for creating the entity
-   /// in an auditable system.
-   /// </summary>
-   string? CreatedBy { get; set; }
+public interface IUserAuditableEntity : IUserAuditableEntity<string>;
 
-   /// <summary>
-   /// Gets or sets the identifier of the user who last updated the entity.
-   /// This property is used for tracking the user responsible for the most recent modification
-   /// in an auditable system.
-   /// </summary>
-   string? UpdatedBy { get; set; }
-
-   /// <summary>
-   /// Gets or sets the identifier of the user who deleted the entity.
-   /// This property is used for tracking the user responsible for the deletion event
-   /// as part of the audit process.
-   /// </summary>
-   string? DeletedBy { get; set; }
-}
-
+/// <summary>
+/// Represents an interface that extends the base-auditable entity functionality by including
+/// user-specific auditing capabilities for tracking changes made by users.
+/// </summary>
+public interface IUserAuditableEntity<T>
+   : IAuditableEntity, IUserCreatedAuditableEntity<T>, IUserUpdatedAuditableEntity<T>, IUserDeletedAuditableEntity<T>
+   where T : IComparable<T>, IEquatable<T>;
