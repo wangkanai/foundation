c4b6bc36f29ad97a5f9387bcc78f8572cce2df8f
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-09-08 11:51:19 +0700

    Refactor entity configuration to streamline primary key property setup and enhance type handling

--- src/Domain/Configurations/EntityConfigurationBuilder.cs
+++ src/Domain/Configurations/EntityConfigurationBuilder.cs
@@ -10,36 +10,24 @@ public static class EntityConfigurationBuilder
       where T : IEquatable<T>, IComparable<T>
    {
       builder.HasKey(x => x.Id);
-      
-      var idProperty = builder.Property(x => x.Id)
-                             .IsRequired()
-                             .ValueGeneratedOnAdd();
 
-      // Configure based on the primary key type
-      ConfigureByKeyType(idProperty);
+      builder.Property(x => x.Id)
+             .IsRequired()
+             .ValueGeneratedOnAdd()
+             .ConfigureByKeyType();
    }
 
-   private static void ConfigureByKeyType<T>(PropertyBuilder<T> property)
+   private static void ConfigureByKeyType<T>(this PropertyBuilder<T> property)
       where T : IEquatable<T>, IComparable<T>
    {
       var keyType = typeof(T);
 
-      switch (Type.GetTypeCode(keyType))
-      {
-         case TypeCode.Int32:
-         case TypeCode.Int64:
-            // Let EF Core handle identity generation based on provider
-            property.ValueGeneratedOnAdd();
-            break;
-            
-         case TypeCode.String:
-            property.HasMaxLength(450);       // Common database index key limit
-            break;
-            
-         case TypeCode.Object when keyType == typeof(Guid):
-            // Client-side GUID generation - database agnostic
-            property.ValueGeneratedOnAdd();
-            break;
-      }
+      _ = Type.GetTypeCode(keyType) switch
+          {
+             TypeCode.Int32 or TypeCode.Int64             => property.ValueGeneratedOnAdd(), // Let EF Core handle identity generation based on provider
+             TypeCode.String                              => property.HasMaxLength(450),     // Common database index key limit
+             TypeCode.Object when keyType == typeof(Guid) => property.ValueGeneratedOnAdd(), // Client-side GUID generation - database agnostic
+             _                                            => property
+          };
    }
 }
