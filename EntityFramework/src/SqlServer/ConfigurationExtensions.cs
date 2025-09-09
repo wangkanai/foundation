04ed052085f5a07802268083d86828d5a2e4f8fa
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-09-09 11:42:07 +0700

    feat: add XML documentation for SqlValueGeneratedOnAdd extension method in ConfigurationExtensions

--- EntityFramework/src/SqlServer/ConfigurationExtensions.cs
+++ EntityFramework/src/SqlServer/ConfigurationExtensions.cs
@@ -5,10 +5,19 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Wangkanai.EntityFramework.SqlServer;
 
+/// <summary>
+/// Provides extension methods for configuring SQL Server-specific behaviors on Entity Framework Core properties.
+/// </summary>
 public static class ConfigurationExtensions
 {
    private const string GetDate = "GETDATE()";
 
+   /// <summary>
+   /// Configures a property to use the SQL Server "GETDATE()" function as the default value and
+   /// to generate its value only when a new entity is added.
+   /// </summary>
+   /// <typeparam name="T">The type of the property being configured.</typeparam>
+   /// <param name="builder">The property builder used to configure the property.</param>
    public static void SqlValueGeneratedOnAdd<T>(this PropertyBuilder<T> builder)
    {
       builder.HasDefaultValueSql(GetDate);
