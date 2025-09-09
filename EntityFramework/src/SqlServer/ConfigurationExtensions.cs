c908b36ad2161cb1ab553338fe436056fa19bc6b
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-09-09 11:34:56 +0700

    feat: add SqlValueGeneratedOnAdd extension method for EF Core property configuration

--- EntityFramework/src/SqlServer/ConfigurationExtensions.cs
+++ EntityFramework/src/SqlServer/ConfigurationExtensions.cs
@@ -1,5 +1,17 @@
 // Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved.
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
 namespace Wangkanai.EntityFramework.SqlServer;
 
-public static class ConfigurationExtensions { }
+public static class ConfigurationExtensions
+{
+   private const string GetDate = "GETDATE()";
+
+   public static void SqlValueGeneratedOnAdd<T>(this PropertyBuilder<T> builder)
+   {
+      builder.HasDefaultValueSql(GetDate);
+      builder.ValueGeneratedOnAdd();
+   }
+}
