876e6f6af94c068b80f3f58579ff2444b0fa7ccb
Sarin Na Wangkanai <10666633+wangkanai@users.noreply.github.com>
2025-09-14 12:33:09 +0700

    refactor: remove unused using directive for Microsoft.EntityFrameworkCore in KeyConfigurationBuilder

--- Foundation/src/Application/Configurations/KeyConfigurationBuilder.cs
+++ Foundation/src/Application/Configurations/KeyConfigurationBuilder.cs
@@ -1,6 +1,5 @@
 // Copyright (c) 2014-2025 Sarin Na Wangkanai, All Rights Reserved.
 
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Wangkanai.Foundation.Configurations;
